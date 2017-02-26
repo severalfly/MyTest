a9f2f52387cbdfa7cd843a89e88203d51e0deb25
severalfly <zhangsmile90@gmail.com>
2017-02-26 12:25:58 +0800

    添加spring beginning ch2

---
+++ springLearning/spring-book-ch2/src/main/java/com/wiley/beginningspring/ch2/AccountDaoJdbcImpl.java
@@ -0,0 +1,48 @@
+package com.wiley.beginningspring.ch2;
+
+import java.util.List;
+
+public class AccountDaoJdbcImpl implements AccountDao
+{
+
+	public void insert(Account account)
+	{
+		
+	}
+
+	public void update(Account account)
+	{
+		
+	}
+
+	public void update(List<Account> accounts)
+	{
+		
+	}
+
+	public void delete(long accountId)
+	{
+		
+	}
+
+	public Account find(long accountId)
+	{
+		return null;
+	}
+
+	public List<Account> find(List<Long> accountIds)
+	{
+		return null;
+	}
+
+	public List<Account> find(String ownerName)
+	{
+		return null;
+	}
+
+	public List<Account> find(boolean locked)
+	{
+		return null;
+	}
+
+}
