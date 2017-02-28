f4309b557413fdae6a8beffab72b06d0213438e1
severalfly <zhangsmile90@gmail.com>
2017-02-28 15:08:46 +0800

    演示线程安全与否的num

--- JavaLearning/JAVA多线程编程核心技术-JAVA Core/src/main/java/org/ch2/t1/HasSelfPrivateNum.java
+++ JavaLearning/JAVA多线程编程核心技术-JAVA Core/src/main/java/org/ch2/t1/HasSelfPrivateNum.java
@@ -3,14 +3,19 @@ package org.ch2.t1;
 public class HasSelfPrivateNum
 {
 	/**
+	 * num 放在方法外面时，非线程安全，可被覆盖
+	 */
+	private int num = 0;
+	/**
 	 * 方法内变量为线程安全
 	 * @param username
 	 */
 	public void addI(String username)
 	{
+		// num 放在方法内时，为线程安全的，
+		//		int num = 0;
 		try
 		{
-			int num = 0;
 			if (username.equals("a"))
 			{
 				num = 100;
@@ -22,7 +27,7 @@ public class HasSelfPrivateNum
 				num = 200;
 				System.out.println("b set over");
 			}
-			System.out.println(username = " num= " + num);
+			System.out.println(username + " num= " + num);
 		}
 		catch (Exception e)
 		{
