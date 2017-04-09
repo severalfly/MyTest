57cb6fbc70a04fb0f7ca9682028a519ceaf54692
leon <zhangsmile90@gmail.com>
2017-04-09 11:13:16 +0800

    1. use with to auto close session 2. add a simple timer

--- pyProTest/tensorflow/learnStart/startTF.py
+++ pyProTest/tensorflow/learnStart/startTF.py
@@ -9,7 +9,7 @@ matrix2 = tf.constant([[2.], [2.]])
 product = tf.matmul(matrix1, matrix2)
 
 
-
+'''
 # start a session
 sess = tf.Session()
 # ->  [[ 12.]]
@@ -18,3 +18,8 @@ res = sess.run(product)
 
 print 'result is: ' 
 print res
+'''
+
+with tf.Session() as sess:
+  result = sess.run(product)
+  print result
