c994413fe1f6848c6638182de8eda1a4b74778a8
severalfly <zhangsmile90@gmail.com>
2017-04-19 20:45:27 +0800

    add main

--- JavaLearning/JAVA多线程编程核心技术-JAVA Core/src/main/java/org/ch3/stack_1/Runn.java
+++ JavaLearning/JAVA多线程编程核心技术-JAVA Core/src/main/java/org/ch3/stack_1/Runn.java
@@ -2,9 +2,15 @@ package org.ch3.stack_1;
 
 public class Runn
 {
-	MyStack stack = new MyStack();
-	P p = new P(stack);
-	C c = new C(stack);
-	P_Thread pThread = new P_Thread(p);
-	C_Thread cThread = new C_Thread(c);
+	public static void main(String[] args)
+	{
+
+		MyStack stack = new MyStack();
+		P p = new P(stack);
+		C c = new C(stack);
+		P_Thread pThread = new P_Thread(p);
+		C_Thread cThread = new C_Thread(c);
+		pThread.start();
+		cThread.start();
+	}
 }
