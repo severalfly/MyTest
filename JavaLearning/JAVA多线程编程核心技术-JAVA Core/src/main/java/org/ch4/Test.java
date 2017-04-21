b1dd0a6d55065dca3486fabb9db3c8676aeee8e7
severalfly <zhangsmile90@gmail.com>
2017-04-21 14:38:28 +0800

    ä¸ä¸ªéº»ç¦çé®é¢ï¼å¤çº¿ç¨å¶å°æ¥é

---
+++ JavaLearning/JAVA多线程编程核心技术-JAVA Core/src/main/java/org/ch4/Test.java
@@ -0,0 +1,88 @@
+package org.ch4;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class Test
+{
+	private volatile Map<String, Integer> map;
+
+	private Map<String, Integer> getMap()
+	{
+		build();
+		return map;
+	}
+
+	private void build()
+	{
+		if (map == null)
+		{
+			synchronized (this)
+			{
+				if (map == null)
+				{
+					try
+					{
+						long sleep = 100;
+						Thread.sleep(sleep);
+						map = new HashMap<String, Integer>();
+						Thread.sleep(sleep);
+						map.put("a0", 0);
+						Thread.sleep(sleep);
+						map.put("a1", 1);
+						Thread.sleep(sleep);
+						map.put("a2", 2);
+						Thread.sleep(sleep);
+						map.put("a3", 3);
+					}
+					catch (InterruptedException e)
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+
+	}
+
+	public void test()
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			final int n = i;
+			new Thread()
+			{
+				public void run()
+				{
+					String key = "";
+					Map<String, Integer> map = getMap();
+					try
+					{
+						key = "a" + n % 4;
+
+						int x = map.get(key);
+						System.out.println(x);
+						//					System.out.println(map.get("a" + n % 4));
+					}
+					catch (Exception e)
+					{
+						System.out.println(Thread.currentThread().getName() + "  " + JSONObject.toJSONString(map));
+						System.out.println(Thread.currentThread().getName() + "  " + "exception " + key);
+						e.printStackTrace();
+						System.exit(0);
+					}
+				};
+			}.start();
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		Test test = new Test();
+		test.test();
+	}
+
+}
