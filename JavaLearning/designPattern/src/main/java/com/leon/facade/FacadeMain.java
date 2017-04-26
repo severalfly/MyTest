434beee2fac588f2094cc1a731c623fe539acaf2
severalfly <zhangsmile90@gmail.com>
2017-04-26 23:07:49 +0800

    模版模式

---
+++ JavaLearning/designPattern/src/main/java/com/leon/facade/FacadeMain.java
@@ -0,0 +1,11 @@
+package com.leon.facade;
+
+public class FacadeMain
+{
+	public static void main(String[] args)
+	{
+		Facade facade = new Facade();
+		facade.methodA();
+		facade.methodB();
+	}
+}
---
+++ JavaLearning/designPattern/src/main/java/com/leon/facade/SubSystem1.java
@@ -0,0 +1,9 @@
+package com.leon.facade;
+
+public class SubSystem1
+{
+	public void method1()
+	{
+		System.out.println("method1");
+	}
+}
