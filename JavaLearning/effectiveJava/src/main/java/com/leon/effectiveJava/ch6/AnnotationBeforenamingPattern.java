e3646e56bee2847fc4e2b3996dd3a3ac8b4d57c7
severalfly <zhangsmile90@gmail.com>
2017-05-28 14:23:28 +0800

    add ch6

---
+++ JavaLearning/effectiveJava/src/main/java/com/leon/effectiveJava/ch6/AnnotationBeforenamingPattern.java
@@ -0,0 +1,12 @@
+package com.leon.effectiveJava.ch6;
+
+import java.util.Date;
+
+public class AnnotationBeforenamingPattern
+{
+	public static void main(String[] args)
+	{
+		Date date = new Date();
+		System.out.println(date.getTime());
+	}
+}
---
+++ JavaLearning/effectiveJava/src/main/java/com/leon/effectiveJava/ch6/EnumInsteadIntege.java
@@ -0,0 +1,9 @@
+package com.leon.effectiveJava.ch6;
+
+public class EnumInsteadIntege
+{
+	public static void main(String[] args)
+	{
+		System.out.printf("i want to test printf %d, add a  %n", 1);
+	}
+}
