29f93ecc021dddb97064dc6efffc4424ae3017ac
severalfly <zhangsmile90@gmail.com>
2017-05-29 20:15:24 +0800

    添加直接获取bean 方法

--- JavaLearning/designPattern/src/main/java/com/leon/util/XMLUtil.java
+++ JavaLearning/designPattern/src/main/java/com/leon/util/XMLUtil.java
@@ -11,18 +11,46 @@ import org.w3c.dom.NodeList;
 
 public class XMLUtil
 {
+	private static final String configPath = "target/classes/config.xml";
 	public static String getCharttype()
 	{
 		try
 		{
 			DocumentBuilderFactory dFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder builder = dFactory.newDocumentBuilder();
-			Document doc = builder.parse(new File("target/classes/config.xml"));
+			Document doc = builder.parse(new File(configPath));
 
 			NodeList nl = doc.getElementsByTagName("chartType");
 			Node classNode = nl.item(0).getFirstChild();
 			String chartType = classNode.getNodeValue().trim();
 			return chartType;
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * 获取指定对象的bean
+	 * @param className
+	 * @return
+	 */
+	public static Object getBean(String className)
+	{
+		try
+		{
+			DocumentBuilderFactory dFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = dFactory.newDocumentBuilder();
+			Document doc = builder.parse(new File(configPath));
+
+			NodeList nl = doc.getElementsByTagName(className);
+			Node classNode = nl.item(0).getFirstChild();
+			String chartType = classNode.getNodeValue().trim();
+
+			Class c = Class.forName(chartType);
+			return c.newInstance();
 		}
 		catch (Exception e)
 		{
