40e19e4faf8720a64017bf862149c444e96533e0
severalfly <zhangsmile90@gmail.com>
2017-06-11 18:18:30 +0800

    memento pattern code

---
+++ JavaLearning/designPattern/src/main/java/com/leon/artofpattern/memento/MementoCaretaker.java
@@ -0,0 +1,17 @@
+package com.leon.artofpattern.memento;
+
+public class MementoCaretaker
+{
+	private ChessmanMemeto memeto;
+
+	public ChessmanMemeto getMemeto()
+	{
+		return this.memeto;
+	}
+
+	public void setMemeto(ChessmanMemeto memeto)
+	{
+		this.memeto = memeto;
+	}
+
+}
