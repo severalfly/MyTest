3f1640e9b17e995b483684a47c5d67c7ec2032d9
severalfly <zhangsmile90@gmail.com>
2017-06-11 09:49:35 +0800

    mediator first start

---
+++ JavaLearning/designPattern/src/main/java/com/leon/artofpattern/mediator/Component.java
@@ -0,0 +1,6 @@
+package com.leon.artofpattern.mediator;
+
+public class Component
+{
+
+}
---
+++ JavaLearning/designPattern/src/main/java/com/leon/artofpattern/mediator/Mediator.java
@@ -0,0 +1,6 @@
+package com.leon.artofpattern.mediator;
+
+public abstract class Mediator
+{
+	public abstract void componentChanged(Component c);
+}
