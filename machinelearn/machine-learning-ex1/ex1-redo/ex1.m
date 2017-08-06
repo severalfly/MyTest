7d03ba354dd69161a23b73eaeb5805d2c83cf005
severalfly <zhangsmile90@gmail.com>
2017-08-06 17:23:35 +0800

    add more data for trainning

--- machinelearn/machine-learning-ex1/ex1-redo/ex1.m
+++ machinelearn/machine-learning-ex1/ex1-redo/ex1.m
@@ -39,12 +39,13 @@ fprintf('Program paused. Press enter to continue.\n');
 %% ======================= Part 2: Plotting =======================
 fprintf('Plotting Data ...\n')
 data = load('ex1data1.txt');
+% data = load('ex1redo.txt');
 X = data(:, 1); y = data(:, 2);
 m = length(y); % number of training examples
 
 % Plot Data
 % Note: You have to complete the code in plotData.m
-% plotData(X, y);
+plotData(X, y);
 
 fprintf('Program paused. Press enter to continue.\n');
 % pause;
