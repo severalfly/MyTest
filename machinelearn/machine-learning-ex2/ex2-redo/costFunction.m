8c35757eed5009184a5a47a7de4435de10f9a512
severalfly <zhangsmile90@gmail.com>
2017-08-06 21:10:04 +0800

    predict

--- machinelearn/machine-learning-ex2/ex2-redo/costFunction.m
+++ machinelearn/machine-learning-ex2/ex2-redo/costFunction.m
@@ -20,7 +20,9 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
-
+h = sigmoid(X * theta);
+J = m^(-1) * sum(-y' * log(h) - (1-y)' * log(1-h));
+grad = m^(-1) * ((h-y)' * X);
 
 
 
