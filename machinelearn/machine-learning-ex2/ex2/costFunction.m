32d15ef22af646e869f684dc906940f91c841bf1
severalfly <zhangsmile90@gmail.com>
2017-08-07 08:52:19 +0800

    cost

--- machinelearn/machine-learning-ex2/ex2/costFunction.m
+++ machinelearn/machine-learning-ex2/ex2/costFunction.m
@@ -20,10 +20,13 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+    % temp0 = theta(1) - alpha * sum(X(:,1)' * ( X  * theta- y)) / m;
+    % temp1 = theta(2) - alpha * sum(X(:,2)' * ( X  * theta- y)) / m;
+    % theta = [temp0; temp1];
 
 
 
-
+	J_history(iter) = computeCost(X, y, theta);
 
 
 
