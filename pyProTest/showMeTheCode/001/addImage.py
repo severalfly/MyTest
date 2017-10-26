e995c0aa50df056515f6fdc1590f5b3113c20164
severalfly <zhangsmile90@gmail.com>
2017-10-26 22:17:42 +0800

    右上角

--- pyProTest/showMeTheCode/001/addImage.py
+++ pyProTest/showMeTheCode/001/addImage.py
@@ -5,3 +5,13 @@ dr = ImageDraw.Draw(img, mode=None)
 font = ImageFont.truetype('simkai.ttf', 50)
 dr.text((0,0), 'hello world', (255,0,255), font=font)
 img.save('out.png')
+
+# 下面准备把字放在右上角
+
+h = 20
+img = Image.open('test.png', mode="r")
+dr = ImageDraw.Draw(img, mode=None)
+x, y = img.width, img.height
+font = ImageFont.truetype(font='simkai.ttf', size=h, index=0, encoding="", layout_engine=None)
+dr.text((x - h,0), '8', fill=(255,0,255), font=font, anchor=None)
+img.save('out.png')
