f0e27419277c37128b39503a62eb5f03cd74c273
severalfly <zhangsmile90@gmail.com>
2017-10-27 00:14:50 +0800

    代码过滤

--- pyProTest/showMeTheCode/007/codeLines.py
+++ pyProTest/showMeTheCode/007/codeLines.py
@@ -15,9 +15,38 @@ def tree(directory):
             tree(path)
         elif os.path.isfile(path):
             print('file ' + path)
+            # lines(path)
         else:
         	print('else ' + path)
 
+def lines(filename):
+    file = open(filename, mode='r', buffering=-1, encoding='utf-8', errors=None, newline=None, closefd=True)
+    inside = False  # 是否在段注释中
+    x , y, z =0,0,0 # x 代码；y 注释；z 空行
+    for line in file.readlines():
+        # print(type(line))
+        line = line.strip()
+        if not line:
+            z +=1
+        elif inside:
+            y +=1
+        elif str(line).find('/*') > 0:
+            y += 1
+            inside = True
+        elif str(line).find('*/') > 0:
+            y +=1
+            inside = False
+        elif str(line).find('//') > 0:
+            y += 1
+        else:
+            x += 1
+
+    return (x,y,z)
+
+
+
 if __name__ == '__main__':
 	# tree('.')
-    tree(r'D:\\leontest')
+    # tree(r'D:\\leontest\\MyTest\\pyProTest\\pythonBase')
+    print(lines('D:\\leontest\\MyTest\\pyProTest\\showMeTheCode\\007\\test.java'))
+    # print(str('aaaatta').index('tt'))
---
+++ pyProTest/showMeTheCode/007/test.java
@@ -0,0 +1,12 @@
+adf
+// asdifha
+/* adoiqwe
+
+*/
+//
+xxxx
+yy
+zzz
+
+
+4 行注释，1 空行，5 正文
