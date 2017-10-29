9b177069ce1d208583ff3861c77f8e8c5f225c34
severalfly <zhangsmile90@gmail.com>
2017-10-29 21:46:23 +0800

    interceptor

---
+++ JavaLearning/leonWechart/src/main/java/com/leon/wechart/interceptor/LogInterceptor.java
@@ -0,0 +1,25 @@
+package com.leon.wechart.interceptor;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.alibaba.fastjson.JSONObject;
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+public class LogInterceptor extends AbstractInterceptor
+{
+	private static final long serialVersionUID = -6589328681383012679L;
+	private static final Logger logger = LoggerFactory.getLogger(LogInterceptor.class);
+
+	@Override
+	public String intercept(ActionInvocation invocation) throws Exception
+	{
+		logger.info(new Date() + " request " + JSONObject.toJSONString(invocation));
+		return Action.SUCCESS;
+	}
+
+}
