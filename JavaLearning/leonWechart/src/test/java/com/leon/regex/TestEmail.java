41d8f7e121b776d377c42aab8465d293b410b4c2
severalfly <zhangsmile90@gmail.com>
2017-11-13 23:07:50 +0800

    regex

---
+++ JavaLearning/leonWechart/src/test/java/com/leon/regex/TestEmail.java
@@ -0,0 +1,20 @@
+package com.leon.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TestEmail
+{
+	public static void main(String[] args)
+	{
+		String str = "-1234). by email(wwspam_a@wwspam.fu) or visiting (http://wwspam.fu/feedback).";
+		Pattern pattern = Pattern.compile("([a-zA-Z_]+?)@([a-zA-Z_]+?\\.[a-zA-Z]{2,})");
+		Matcher matcher = pattern.matcher(str);
+		matcher.find();
+		//		System.out.println(matcher.group(0));
+		//		System.out.println(matcher.group(1));
+		//		System.out.println(matcher.group(2));
+		//		System.out.println(matcher.group(3));
+		System.out.println(matcher.replaceAll("$1#$2"));
+	}
+}
