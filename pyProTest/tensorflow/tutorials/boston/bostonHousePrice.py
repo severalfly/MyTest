b68fc9ab219f28708c3eec3c445979d805fd1bed
severalfly <zhangsmile90@gmail.com>
2017-12-24 09:23:48 +0800

    添加注释

--- pyProTest/tensorflow/tutorials/boston/bostonHousePrice.py
+++ pyProTest/tensorflow/tutorials/boston/bostonHousePrice.py
@@ -5,23 +5,14 @@ import pandas as pd
 import itertools
 tf.logging.set_verbosity(tf.logging.INFO)
 
-# COLUMNS = ['crim', 'zn', 'indus', 'nox', 'rm',
-#            'age', 'dis', 'tax', 'ptratio', 'medv']
-# FEATURES = ['crim', 'zn', 'indus', 'nox', 'rm', 'age', 'dis', 'tax', 'ptratio']
-# LABEL = 'medv'
-# training_set = pd.read_csv('boston_train.csv', skipinitialspace=True,
-#                            skiprows=1, names=COLUMNS)
-# test_set = pd.read_csv('boston_test.csv', skipinitialspace=True,
-#                        skiprows=1, names=COLUMNS)
-# prediction_set = pd.read_csv('boston_predict.csv', skipinitialspace=True,
-#                              skiprows=1, names=COLUMNS)
-
+# 
 COLUMNS = ["crim", "zn", "indus", "nox", "rm", "age",
            "dis", "tax", "ptratio", "medv"]
 FEATURES = ["crim", "zn", "indus", "nox", "rm",
             "age", "dis", "tax", "ptratio"]
 LABEL = "medv"
 
+# import data
 training_set = pd.read_csv("boston_train.csv", skipinitialspace=True,
                            skiprows=1, names=COLUMNS)
 test_set = pd.read_csv("boston_test.csv", skipinitialspace=True,
@@ -30,25 +21,14 @@ prediction_set = pd.read_csv("boston_predict.csv", skipinitialspace=True,
                              skiprows=1, names=COLUMNS)
 
 
-# feature_cols = [tf.feature_column.numeric_column(k) for k in FEATURES]
 feature_cols = [tf.feature_column.numeric_column(k) for k in FEATURES]
 
-# regressor = tf.estimator.DNNRegressor(feature_columns=feature_cols,
-#                                       hidden_units=[10, 10],
-#                                       model_dir='/tmp/boston_model')
+# dnn to train
 regressor = tf.estimator.DNNRegressor(feature_columns=feature_cols,
                                       hidden_units=[10, 10],
                                       model_dir="/tmp/boston_model")
 
-# def get_input_fn(data_set, num_epochs=None, shuffle=True):
-#     return tf.estimator.inputs.pandas_input_fn(
-#         x=pd.DataFrame({k: data_set[k].values for k in FEATURES}),
-#         y=pd.Series(data_set[LABEL].values),
-#         num_epochs=num_epochs,
-#         shuffle=shuffle
-#     )
-
-
+# the core function
 def get_input_fn(data_set, num_epochs=None, shuffle=True):
     return tf.estimator.inputs.pandas_input_fn(
         x=pd.DataFrame({k: data_set[k].values for k in FEATURES}),
@@ -56,13 +36,21 @@ def get_input_fn(data_set, num_epochs=None, shuffle=True):
         num_epochs=num_epochs,
         shuffle=shuffle)
 
-
-# regressor.train(input_fn=get_input_fn(training_set), steps=5000)
+# start to train
 regressor.train(input_fn=get_input_fn(training_set), steps=5000)
 
 
+# evalute the model
 ev = regressor.evaluate(
     input_fn=get_input_fn(test_set, num_epochs=1, shuffle=False)
 )
+
+# result
 loss_score = ev['loss']
 print('Loss: {0:f}'.format(loss_score))
+
+# have a try
+y = regressor.predict(
+    input_fn=get_input_fn(prediction_set, num_epochs=1, shuffle=False))
+predictions = list(p["predictions"] for p in itertools.islice(y, 6))
+print("Predictions: {}".format(str(predictions)))
